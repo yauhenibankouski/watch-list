ead3e2fcbb792c9b0fc33dd6def4d65634c43c2a
Yauheni Bankouski <yauheni.bankouski@gmail.com>
2022-01-27 19:53:37 +0100

    completed front

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
-  validates_uniqueness_of :movie_id, { scope: :list_id }
+  validates_uniqueness_of :movie_id, { scope: :list_id, message: "is already in the list" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :create, :index, :show, :new, :destroy ] do
+    resources :bookmarks, only: [ :create, :index, :show, :new, :destroy ]
+  end
+  resources :bookmarks, only: [ :destroy ]
 end
